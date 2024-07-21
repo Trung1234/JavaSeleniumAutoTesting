3241a210714eae756a1b400614372bf5bc1849fb
Trung1234 <trungsieunhan24@gmail.com>
2024-07-21 16:24:06 +0700

    upload file testcase

--- TestNGExample/src/main/java/testng/Main.java
+++ TestNGExample/src/main/java/testng/Main.java
@@ -2,6 +2,10 @@ package testng;
 
 import static org.testng.Assert.assertEquals;
 
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import testng.setup.SetupBrowser;
@@ -21,4 +25,16 @@ public class Main extends SetupBrowser {
 
 	}
 
+	 @Test
+	  public void fileUploadTest() {
+		 driver.get("https://the-internet.herokuapp.com/upload");
+	    File uploadFile = new File("src/test/resources/selenium-snapshot.png");
+
+	    WebElement fileInput = driver.findElement(By.cssSelector("input[type=file]"));
+	    fileInput.sendKeys(uploadFile.getAbsolutePath());
+	    driver.findElement(By.id("file-submit")).click();
+
+	    WebElement fileName = driver.findElement(By.id("uploaded-files"));
+	    assertEquals("selenium-snapshot.png", fileName.getText());
+	  }
 }

f7eee5893e9c87b643e923c7a49ff505d652af90
Trung1234 <trungsieunhan24@gmail.com>
2024-08-17 05:08:06 +0700

    update homework 2 ex 4

--- Homework2/src/test/java/Exercise4Old.java
+++ Homework2/src/test/java/Exercise4Old.java
@@ -7,16 +7,17 @@ import org.testng.annotations.Test;
 
 import common.SetupBrowser;
 
-public class Exercise4 extends SetupBrowser {
+public class Exercise4Old extends SetupBrowser {
 	@Test
 	public void executeTest() throws InterruptedException {
 
 		// Navigate to the webpage
-		driver.get("https://demoqa.com/frames");
+		driver.get("https://www.w3schools.com/html/tryit.asp?filename=tryhtml_iframe_height_width");
 
 		// Switch to the iframe
-		driver.switchTo().frame("frame1");
-
+		driver.switchTo().frame("iframeResult");
+		// Switch to the child iframe
+		driver.switchTo().frame(0);
 
 		// Get and check the expected text
 		WebElement textElement = driver.findElement(By.tagName("h1"));
@@ -26,7 +27,7 @@ public class Exercise4 extends SetupBrowser {
 		} else {
 			System.out.println("Expected text not found!");
 		}
-		assertEquals(actualText, "This is a sample page");
+		assertEquals(actualText, "This page is displayed in an iframe");
 
 	}
 }

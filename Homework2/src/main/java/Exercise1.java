17270258802bcc6596d40a549fc6c5117e0aa491
Trung1234 <trungsieunhan24@gmail.com>
2024-08-17 15:10:44 +0700

    fix ex 1

--- Homework2/src/main/java/Exercise1.java
+++ Homework2/src/main/java/Exercise1.java
@@ -1,28 +1,24 @@
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.UnreachableBrowserException;
 
 public class Exercise1  {
     private static WebDriver driver;
 
     public static void main(String[] args) throws InterruptedException{
-        setUpDriver();
-        //Element which needs to drag.
-//        WebElement From=driver.findElement(By.xpath("//*[@id='credit0']/a"));
-//
-//        //Element on which need to drop.
-//        WebElement To=driver.findElement(By.id("shoppingCart4"));
-
-        //Element which needs to drag.
+        setUpDriver();       
         try {
+        	//Element which needs to drag.
         	WebElement From=driver.findElement(By.xpath("//*[@id='fourth']/a"));
 
             //Element on which need to drop.
             WebElement To=driver.findElement(By.xpath("//*[@id='amt7']/li"));
-            //WebElement To=driver.findElement(By.xpath("//*[@id='bank']/li"));
             //Using Action class for drag and drop.
             Actions act=new Actions(driver);
 
@@ -41,6 +37,7 @@ public class Exercise1  {
         // Set the path to the chromedriver executable if needed
         System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("https://demo.guru99.com/test/drag_drop.html");
         //Mazimize current window
         driver.manage().window().maximize();
@@ -56,10 +53,18 @@ public class Exercise1  {
      * Method to close the drive
      */
     public static void closeDriver() {
-        if (driver != null) {
-            driver.quit();
-            driver = null;
-        }
+    	try {
+    		Runtime.getRuntime().addShutdownHook(new Thread() {
+    		    public void run() {
+    		        if (driver != null) {
+    		            driver.quit();
+    		        }
+    		    }
+    		});
+		} catch (UnreachableBrowserException e) {
+			// Log the exception
+			System.out.println("Browser was unreachable");
+		}
     }
 
 }

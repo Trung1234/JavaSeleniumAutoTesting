e1d5bed2897749920f8ef39a4003de90bf6b6eb6
Trung1234 <trungsieunhan24@gmail.com>
2024-09-07 11:08:07 +0700

    fix by commit

--- DataProviderPractice/src/test/java/tests/SaunaDemoTest.java
+++ DataProviderPractice/src/test/java/tests/SaunaDemoTest.java
@@ -35,33 +35,17 @@ public class SaunaDemoTest extends BaseTest {
 
 	@DataProvider(name = "loginData")
 	public Object[][] getDataLogin() {
-		Object[][] data = new Object[][];
-
-		// blank user, blank pass
-		data[0][0] = "";
-		data[0][1] = "";
-		data[0][2] = ResultCode.BAD_REQUEST.getMessage();
-
-		// invalid user, valid pass
-		data[1][0] = "invalidUser";
-		data[1][1] = "secret_sauce";
-		data[1][2] = ResultCode.NOT_FOUND.getMessage();
-
-		// invalid pass, valid user
-		data[2][0] = "standard_user";
-		data[2][1] = "invalidPass";
-		data[2][2] = ResultCode.NOT_FOUND.getMessage();
-
-		// valid user, valid pass
-		data[3][0] = "standard_user";
-		data[3][1] = "secret_sauce";
-		data[3][2] = ResultCode.SUCCESS.getMessage();
-
 //		// invalid pass, invalid user
 //		data[4][0] = "standard_user";
 //		data[4][1] = "invalidPass";
 //		data[4][2] = ResultCode.NOT_FOUND.getMessage();
-		return data;
-
+//		return data;
+		return new Object[][]
+                {
+                        {"", "", ResultCode.BAD_REQUEST.getMessage()},
+                        {"invalidUser", "secret_sauce",ResultCode.NOT_FOUND.getMessage()},
+                        {"standard_user", "ResultCode",ResultCode.NOT_FOUND.getMessage() },
+                        {"standard_user", "secret_sauce",ResultCode.SUCCESS.getMessage()}
+                };
 	}
 }

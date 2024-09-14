54fafcaf9b49413b8f076d591908cbdf3d8be948
Trung1234 <trungsieunhan24@gmail.com>
2024-09-14 12:19:34 +0700

    add final test

---
+++ FinalTest/src/test/java/core/BaseTest.java
@@ -0,0 +1,45 @@
+package core;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeTest 
+    @Parameters("browser")
+    public void beforeSuite(String browser) {
+    	if (browser.equalsIgnoreCase("chrome"))  {
+    		WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+    	}
+    	if (browser.equalsIgnoreCase("firefox"))  {
+    		 WebDriverManager.firefoxdriver().setup();
+             driver = new FirefoxDriver();
+    	}
+    }
+
+    @AfterTest
+    public void afterSuite() {
+        if (driver != null) {
+            driver.close();
+            driver.quit();
+        }
+    }
+
+    public void goToLink(String url) {
+        driver.get(url);
+    }
+    
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+}
